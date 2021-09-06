ad542aaf0b8e454b5600daeeebc26def7c49263a
TalentDevking <talentdev311@outlook.com>
2021-09-06 22:06:33 +0200

    feat: pwa functionality added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const path = require('path');
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
 		domains: [
@@ -7,4 +11,25 @@ module.exports = {
       '*amazonaws.com'
 		],
 	},
-}
+  pwa: {
+    dest: 'public',
+    runtimeCaching
+  },
+
+  // This is not required to make it into a PWA, but is a nice way to clean up your imports
+  webpack: (config) => {
+    config.resolve.modules.push(path.resolve('./'));
+    return config;
+  }
+});
+
+// module.exports = {
+//   reactStrictMode: true,
+//   images: {
+// 		domains: [
+// 			'api.nomics.com',
+//       's3.us-east-2.amazonaws.com',
+//       '*amazonaws.com'
+// 		],
+// 	},
+// }
---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<meta
+						name="description"
+						content="Build a Next.js PWA with 100% lighthouse score"
+					></meta>
+					<meta name="theme-color" content="#0D47A1" />
+					<link rel="manifest" href="/manifest.json" />
+					<link rel="apple-touch-icon" href="/apple-icon.png"></link>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
